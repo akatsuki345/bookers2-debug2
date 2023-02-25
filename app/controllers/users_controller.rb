51b4e5dc8b9dd2d8312785662ece304c1ba9af8c
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-02-25 13:44:02 +0000

    [interruption]bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path(@user.id), notice: "You have updated user successfully."
     else
       render "edit"
     end
@@ -33,10 +34,17 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
+  def is_matching_login_user
+    @user_id = params[:id].to_i
+    unless @user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to user_path(current_user)
+      redirect_to user_path(current_user.id)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  root to: "homes#top"
+
+  get 'home/top' => 'home#top'
+  root to: "home#top"
   devise_for :users
 
    resources :books
    resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   get "homes/about"=>"homes#about", as: "about"
+  get 'home/about'=>"home#about", as: "about"
 end
