7d1d16610d08294fab57ad57a32d323af0dcf25a
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-01 09:42:45 +0000

    [interruption]bookers2-debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_26_023555) do
+ActiveRecord::Schema.define(version: 2023_03_01_081553) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2023_02_26_023555) do
     t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
