a436e843b4770d5cb6bf7090665c544ee0eba3fd
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-02 07:52:00 +0000

    [interruption]bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
   end
 
   def index
     @users = User.all
     @book = Book.new
     @user = current_user
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def edit
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   end
 
   def followers
-    user = User.find(params[:id])
+   user = User.find(params[:id])
     @users = user.follower_user.page(params[:page]).per(3).reverse_order
   end
 
