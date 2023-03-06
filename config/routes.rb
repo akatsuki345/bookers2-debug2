0cac494e4e5716669308339206ddd55830f1cbcc
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-06 08:45:43 +0000

    [Finish]bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
 
+  get 'messages/create'
+  get 'rooms/create'
+  get 'rooms/index'
+  get 'rooms/show'
   get 'home/top' => 'home#top'
   get "search" => "searches#search"
   root to: "home#top"
-  devise_for :users
+
 
    resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
    end
 
+   devise_for :users
    resources :users do
     member do
       get :follows, :followers
     end
     resource :relationships, only: [:create, :destroy]
+    resources :messages, only: [:create]
+    resources :rooms, only: [:create, :index, :show]
    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/about'=>"home#about", as: "about"
