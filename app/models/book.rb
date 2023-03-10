888f726d8b8e71cd579397e5c049ae968dadbdb3
EC2 Default User <ec2-user@ip-172-31-3-77.ec2.internal>
2023-03-10 06:54:47 +0000

    [Finish応用課題7b]bookers2-debug2

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,11 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :view_counts, dependent: :destroy
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: Time.zone.now.all_week) }
+  scope :created_last_week, -> { where(created_at: 1.week.ago.all_week) }
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
